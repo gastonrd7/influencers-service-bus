5ce2e6c61258654022e00f4213023896bf5522c2
gastonrd7 <gaston.rd7@gmail.com>
2019-04-01 11:44:50 -0300

    subida codigo

--- src/services/serviceBus/constants/socialMedia/contentTypes/facebook/post.ts
+++ src/services/serviceBus/constants/socialMedia/contentTypes/facebook/post.ts
@@ -1,20 +1,32 @@
 export class FacebookPost {
     constructor(){}
-    comments: FacebookComment[];
-    shares: FacebookShares[];
-    likes: FacebookReactions[];
+    insigths: Insigth[];
+    // comments: FacebookComment[];
+    // shares: FacebookShares[];
+    // likes: FacebookReactions[];
     link: String
 }
 
-export class FacebookShares {
-    constructor(){}
-    userName: String;
+export enum InsigthType {
+    COMMENT = "COMMENT",
+    SHARE = "SHARE",
+    LIKE = "LIKE"
 }
-export class FacebookReactions {
-    constructor(){}
-        userName: String;
+
+export class Insigth {
+    constructor(type: InsigthType, userName: String){}
 }
-export class FacebookComment {
-        constructor(){}
-        userName:String;
-};
+
+// export class FacebookShares {
+//     constructor(){}
+//     userName: String;
+// }
+// export class FacebookReactions {
+//     constructor(){}
+//         userName: String;
+// }
+// export class FacebookComment {
+//         constructor(){}
+//         userName:String;
+// };
+
