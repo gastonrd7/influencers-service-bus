6ef642cc5aae98046b438c8e63e2d4b7dd2b5789
Mario Ruiz Diaz <mariogrd@gmail.com>
2020-05-19 18:30:31 -0300

    fields renaming

--- src/services/serviceBus/constants/socialMedia/contentTypes/SocialMediaRelationship.ts
+++ src/services/serviceBus/constants/socialMedia/contentTypes/SocialMediaRelationship.ts
@@ -1,5 +1,5 @@
 import { people_relationshipEnum } from "influencers-models";
 
 export class SocialMediaRelationship {
-    constructor(public platformObjectId: string, public avatarUrl: string, public displayName: string, public relationship: people_relationshipEnum){}
+    constructor(public platformObjectIdentity: string, public picture: string, public displayName: string, public relationship: people_relationshipEnum){}
 }
